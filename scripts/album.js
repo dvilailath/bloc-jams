04ff3667b1556091d23c030ea43f60a19e5c761f
dvilailath <dvilailath@github.com>
2017-05-24 21:05:36 -0500

    Assignment 19 next/prev:

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,12 @@
+var setSong = function(songNumber) {
+ currentlyPlayingSongNumber = parseInt(songNumber);
+ currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+};
+
+var getSongNumberCell = function(number) {
+    return $('.song-item-number[data-song-number="' + number + '"]');
+};
+
 var createSongRow = function (songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
@@ -8,39 +17,27 @@ var createSongRow = function (songNumber, songName, songLength) {
  
     var $row = $(template);
 
-var setSong = function(songNumber) {
-    var currentPlayingSongNumber = parseInt(songNumber);
-    var currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
-};
-    
-    
-var getSongNumberCell = function(number) {
-    return $('.song-item-number[data-song-number="' + number + '"]');
-};
-    
  var clickHandler = function() {
-
+    
      var songNumber = parseInt($(this).attr('data-song-number'));
 
-     if (setSong(songNumber) !== null) {
+     if (currentlyPlayingSongNumber !== null) {
          // Revert to song number for currently playing song because user started playing new song.
 
-         var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+         var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
          currentlyPlayingCell.html(currentlyPlayingSongNumber);
      }
 
-     if (setSong(songNumber) !== songNumber) {
+     if (currentlyPlayingSongNumber !== songNumber) {
          // Switch from Play -> Pause button to indicate new song is playing.
          $(this).html(pauseButtonTemplate);
-         currentlyPlayingSongNumber = songNumber;
-         currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+         setSong(songNumber);
          updatePlayerBarSong();
-      } else if (setSong(songNumber) === songNumber) {
+      } else if (currentlyPlayingSongNumber === songNumber) {
          // Switch from Pause -> Play button to pause currently playing song.
          $(this).html(playButtonTemplate);
          $('.main-controls .play-pause').html(playerBarPlayButton);
-         currentlyPlayingSongNumber = null;
-         currentSongFromAlbum = null;
+         setSong(songNumber) = null;
      }
 
  };
